651d941983fce3eca3e3fae6cacfc5f9409cc7d1
Javier Sánchez Vázquez de Mercado <a00517066@itesm.mx>
2023-06-01 20:16:59 -0600

    Eliminación de comentarios

--- directorioProcedimientos.py
+++ directorioProcedimientos.py
@@ -11,19 +11,9 @@ class Directorio:
 
     def agregarVariables(self, variable, tipo, nombreFuncion):
         if variable in self.tabla[nombreFuncion]["tablaVariables"].keys():
-            #print('La variable '+ variable + " ya existe. Favor de renombrala.")
             raise Exception("La variable "+ variable + " ya existe. Favor de renombrala.")
         else:
             self.tabla[nombreFuncion]["tablaVariables"][variable] = tipo
-
-    # def quitarVariable(self, variable, nombreFuncion):
-    #     self.tabla[nombreFuncion]["Tabla de variables"].pop(variable)
-    
-    # def existeVariable(self, variable, nombreFuncion):
-    #     if variable in self.tabla[nombreFuncion]["Tabla de variables"].keys():
-    #         return True
-    #     else:
-    #         return False
             
     def agregarFuncion(self, nombreFuncion, tipoFuncion):
         if nombreFuncion in self.tabla.keys():
