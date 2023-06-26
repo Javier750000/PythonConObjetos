146eccc2ed92ea9ee772c412643e2c8124117b5a
Javier Sánchez Vázquez de Mercado <a00517066@itesm.mx>
2023-06-26 11:46:28 -0600

    Operadores aritméticos pendientes
    Se incluyeron los operadores aritméticos que estaban pendientes en la ejecución de la máquina virtual al recorrer los cuádruplos con el apuntador de instrucciones. También se mejoraron las escrituras para que fueran más legibles.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from yacc import parser
 
 def main():
-    print("Escriba el nombre del archivo de prueba.")
+    #print("Escriba el nombre del archivo de prueba.")
     nombreArchivo = "Archivo.txt"
     print("")
     archivo = open(nombreArchivo)
     datos = archivo.read()
     archivo.close()
     parser.parse(datos)
-    print("El código pudo ser leído correctamente.")
 
 if __name__ == '__main__':
     main()
--- maquinaVirtual.py
+++ maquinaVirtual.py
@@ -1,11 +1,10 @@
 from glob import glob
-import pprint
+from pprint import pprint
 from constantes import Constantes
 from directorioProcedimientos import Directorio
 from cuadruplos import Cuadruplos
 
 class MaquinaVirtual:
-
     def __init__(self, _id):
         self.memoria = {}
         self.id = _id
@@ -13,64 +12,62 @@ class MaquinaVirtual:
     def insertar(self, dir, valor, tipo):
         self.memoria[dir] = {"valor": valor, "tipo": tipo}
 
-
 global memoriaLocal
 memoriaLocal = MaquinaVirtual(None)
 memoriaGlobal = MaquinaVirtual("global")
 saltosPendientes = []
 
-
-def ejecutar_maquina_virtual(directorioProcedimientos: Directorio, constantes: Constantes,cuadruplos: Cuadruplos):
-    print('""""""""""""""""""""""""""""""""""""""""""""""""""""""')
-    print('""""""""""""""""""""""""""""""""""""""""""""""""""""""')
-    print('""""""""""""""""""""""""""""""""""""""""""""""""""""""')
-    print('""""""""""""""""""""""""""""""""""""""""""""""""""""""')
-    print('""""""""""""""""""""""""""""""""""""""""""""""""""""""')
-    print('""""""""""""""""""""""""""""""""""""""""""""""""""""""')
-    print('Ejecutando maquina virtual')
+def ejecutar_maquina_virtual(directorioProcedimientos: Directorio, constantes: Constantes, cuadruplos: Cuadruplos):
+    print('Ejecutando máquina virtual: ')
+    print("")
     #saltosPendientes.append(len(cuadruplos)-1)
-
     global _directorioProc
     _directorioProc = directorioProcedimientos
-
     apuntadorInstrucciones = 0
-
     for idConst, dataConstante in constantes.items():
-        guardarEnMemoria(dataConstante["direccionVirtual"],idConst,dataConstante["tipoConstante"])
-
-    print("QUADS",cuadruplos.listaCuadruplos)
-
-
-    
+        guardarEnMemoria(dataConstante["direccionVirtual"], idConst, dataConstante["tipoConstante"])
+    print("Cuádruplos: ")
+    i=1
+    for cuadruplo in cuadruplos.listaCuadruplos:
+        print(f'{i}: {cuadruplo}')
+        i+=1;
+    print("")
     while cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] != "End":
         global memoriaLocal
-
         if cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "=":
-            print("ASIG",cuadruplos.listaCuadruplos[apuntadorInstrucciones])
-            apuntadorInstrucciones+=1
-        
-        if cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "+":
-            print("SUMA",cuadruplos.listaCuadruplos[apuntadorInstrucciones])
-            apuntadorInstrucciones+=1
-        
-        if cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "-":
-            print("RESTA",cuadruplos.listaCuadruplos[apuntadorInstrucciones])
-            apuntadorInstrucciones+=1
-
-        if cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "*":
-            print("MULT",cuadruplos.listaCuadruplos[apuntadorInstrucciones])
-            apuntadorInstrucciones+=1
-        
-        if cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "/":
-            print("DIV",cuadruplos.listaCuadruplos[apuntadorInstrucciones])
-            apuntadorInstrucciones+=1
+            print("Asignación:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        if cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "print":
+            print("Escritura:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        if cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "read":
+            print("Lectura:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "+":
+            print("Suma:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "-":
+            print("Resta:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "*":
+            print("Multiplicación:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "/":
+            print("División:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == ">":
+            print("Mayor que:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == ">=":
+            print("Mayor igual que:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "<":
+            print("Menor que:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "<=":
+            print("Menor igual que:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "==":
+            print("Equivalente a:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "<>":
+            print("Diferente de:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "&&":
+            print("And:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
+        elif cuadruplos.listaCuadruplos[apuntadorInstrucciones][0] == "||":
+            print("Or:", cuadruplos.listaCuadruplos[apuntadorInstrucciones])
         apuntadorInstrucciones+=1
-
-    print("memoria global en maquina")
-    print(memoriaGlobal.memoria)
-
-
-
+    print("")
+    print("Memoria global en la máquina virtual: ")
+    pprint(memoriaGlobal.memoria)
 
 def guardarEnMemoria(dir, valor, tipo):
     global memoriaLocalActual
@@ -83,7 +80,6 @@ def esVariableGlobal(dir):
     if (dir >= 0 and dir <= 11999) or (dir >= 36000 and dir <= 47999):
         return True
     return False
-
 
 # def extraerValorPorDir(dir, noAsignada=True):
 #     global memoriaLocal
